4699b452db46379b46c2180ca0bd6429ead5f51b
Willie Antonio <willie.antonio.santos@gmail.com>
2021-06-10 00:19:30 -0300

    "Create PersonDTO and PhoneDTO"

--- src/main/java/com/was/personasapi/dto/mapper/PersonMapper.java
+++ src/main/java/com/was/personasapi/dto/mapper/PersonMapper.java
@@ -1,5 +1,6 @@
 package com.was.personasapi.dto.mapper;
 
+import com.was.personasapi.dto.request.PersonDTO;
 import com.was.personasapi.entity.Person;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -7,11 +8,9 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "string")
 public interface PersonMapper {
-/*
+
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO dto);
 
     PersonDTO toDTO(Person dto);
-
- */
 }
